e708daa7fc182bf03c00b27da03e722b1c80fcab
Anton Drofenik <a93.dro@gmai.com>
2021-04-07 12:19:16 +0200

    change user to object

--- client/src/redux/reducers/user.js
+++ client/src/redux/reducers/user.js
@@ -1,7 +1,7 @@
-const userReducer = (state = [], action) => {
+const userReducer = (state = null, action) => {
   switch (action.type) {
     case "SET_USER": {
-      return [action.payload];
+      return action.payload;
     }
     default:
       return state;
