fd00a3b26c417985f81d4f6bbc92ce1987634f31
Anton Drofenik <a93.dro@gmai.com>
2021-04-07 12:21:00 +0200

    add random user generation

--- client/src/components/chat-window/chat-window.js
+++ client/src/components/chat-window/chat-window.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import setUser from "../../redux/actions/user";
 import setMessages from "../../redux/actions/message-actions";
 import setActiveUsers from "../../redux/actions/active-users-actions";
 
@@ -18,24 +19,43 @@ import ActiveUsers from "./active-users";
 import { Container, Grid } from "@material-ui/core";
 
 const ChatWindow = () => {
+  const dispatch = useDispatch();
+
   const messages = useSelector((state) => state.messages);
   const activeUsers = useSelector((state) => state.activeUsers);
+  const user = useSelector((state) => state.user);
 
   useObservable(messages$, setMessages);
   useObservable(activeUsers$, setActiveUsers);
 
   useEffect(() => {
-    getMessages();
+    if (!user) {
+      const randomUsername = `User.${Math.ceil(Math.random() * 1000)}`;
+      const randomUser = {
+        username: randomUsername,
+        uid: randomUsername,
+        avatar: null,
+      };
+      dispatch(setUser(randomUser));
+
+      getMessages(randomUser);
+    } else {
+      getMessages(user);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <Container>
+    <Container disableGutters>
       <Grid container>
-        <Grid item xs={10}>
-          <Messages messages={messages} sendMessage={sendMessage} />
+        <Grid item xs={12} md={10}>
+          <Messages messages={messages} sendMessage={sendMessage} user={user} />
         </Grid>
-        <Grid item xs={2}>
-          <ActiveUsers activeUsers={activeUsers} />
+        <Grid item xs={false} md={2}>
+          <Container>
+            <ActiveUsers activeUsers={activeUsers} />
+          </Container>
         </Grid>
       </Grid>
     </Container>
