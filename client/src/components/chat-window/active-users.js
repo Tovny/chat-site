7cca4d6e4486eb50f8b9faa71de49041d12e0529
Anton Drofenik <a93.dro@gmai.com>
2021-04-08 17:14:14 +0200

    add duplicate checking

--- client/src/components/chat-window/active-users.js
+++ client/src/components/chat-window/active-users.js
@@ -1,10 +1,43 @@
-const ActiveUsers = ({ activeUsers }) => {
+import { useSelector } from "react-redux";
+import { Avatar, Typography, Grid } from "@material-ui/core";
+
+const ActiveUsers = ({ activeUsers, classes }) => {
+  const thisUser = useSelector((state) => state.user);
+
   return (
-    <div>
-      {activeUsers?.map((user) => (
-        <p>{user}</p>
+    <>
+      {thisUser && (
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          className={classes.activeUserContainer}
+        >
+          <Avatar
+            alt={thisUser.username}
+            src={thisUser.avatar}
+            className={classes.activeUserAvatar}
+          />
+          <Typography variant="body1">{thisUser.username}</Typography>
+        </Grid>
+      )}
+      {activeUsers?.map((user, i) => (
+        <Grid
+          key={i}
+          container
+          direction="row"
+          alignItems="center"
+          className={classes.activeUserContainer}
+        >
+          <Avatar
+            alt={user.username}
+            src={user.avatar}
+            className={classes.activeUserAvatar}
+          />
+          <Typography variant="body1">{user.username}</Typography>
+        </Grid>
       ))}
-    </div>
+    </>
   );
 };
 
