54ccc207aa7aef8f7f9d873a184f1ddf706dc107
Anton Drofenik <a93.dro@gmai.com>
2021-04-08 17:13:16 +0200

    add firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCW4DxXtIJbW7TGq-hEQMnO0ai4VS0oxS4",
+  authDomain: "chat-pwa-fd1fb.firebaseapp.com",
+  projectId: "chat-pwa-fd1fb",
+  storageBucket: "chat-pwa-fd1fb.appspot.com",
+  messagingSenderId: "121873640938",
+  appId: "1:121873640938:web:6330257f091ee15607d693",
+};
+
+export default firebaseConfig;
