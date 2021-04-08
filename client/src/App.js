dad3788ae22eb95e00ee0734a0cc556f3253ffbe
Anton Drofenik <a93.dro@gmai.com>
2021-04-08 17:13:08 +0200

    add firebase login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,40 @@
-import ChatWindow from "./components/chat-window/chat-window";
+import { useEffect } from "react";
 
-import { CssBaseline } from "@material-ui/core";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+import setUser from "./redux/actions/user";
+
+import { useObservable, login$, sendLogin, sendLogout } from "./websocket";
+
+import ChatWindow from "./components/chat-window/Chat-window";
+import Header from "./components/header/Header";
+import Rooms from "./components/Rooms";
+
+import { CssBaseline, Grid } from "@material-ui/core";
 
 function App() {
+  useObservable(login$, setUser);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) sendLogin(user.uid, user);
+    });
+  }, []);
+
   return (
     <>
       <CssBaseline />
       <div className="App">
-        <ChatWindow />
+        <Header />
+        <Grid container direction="row" justify="center">
+          <Grid item xs={2}>
+            <Rooms />
+          </Grid>
+          <Grid item xs={10}>
+            <ChatWindow />
+          </Grid>
+        </Grid>
       </div>
     </>
   );
