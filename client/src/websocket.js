3cdfc9128f176548ecac48052e30ace706c04fb4
Anton Drofenik <a93.dro@gmai.com>
2021-04-08 17:13:35 +0200

    add login

--- client/src/websocket.js
+++ client/src/websocket.js
@@ -21,6 +21,16 @@ export const activeUsers$ = socket$.pipe(
   map((msg) => msg.payload)
 );
 
+export const userLeave$ = socket$.pipe(
+  filter((msg) => msg.type === "userLeft"),
+  map((msg) => msg.payload)
+);
+
+export const login$ = socket$.pipe(
+  filter((msg) => msg.type === "login"),
+  map((msg) => msg.payload)
+);
+
 // ACTIONS
 
 export const getMessages = (user) => {
@@ -29,6 +39,14 @@ export const getMessages = (user) => {
 
 export const sendMessage = (msg, user) => {
   socket$.next({ type: "message", payload: msg, user });
+};
+
+export const sendLogin = (uid, user) => {
+  socket$.next({ type: "login", payload: uid, user });
+};
+
+export const sendLogout = (user) => {
+  socket$.next({ type: "userLeft", user });
 };
 
 // CUSTOM HOOKS
