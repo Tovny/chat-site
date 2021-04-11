2c36c240e9145086cc08bbac66b49533e761490f
Anton Drofenik <a93.dro@gmai.com>
2021-04-11 15:38:24 +0200

    change to display room name

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,13 +3,17 @@ import headerStyles from "./Header-styles";
 
 import UserControl from "./UserControl";
 
+import { useSelector } from "react-redux";
+
 const Header = () => {
   const classes = headerStyles();
+
+  const room = useSelector((state) => state.room);
 
   return (
     <AppBar position="sticky">
       <Toolbar className={classes.headerToolbar}>
-        <Typography variant="h5">Global room</Typography>
+        <Typography variant="h5">{room}</Typography>
         <UserControl />
       </Toolbar>
     </AppBar>
