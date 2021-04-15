5ae7764abc31b76a22e4d965cf38aa28e167f95e
Anton Drofenik <a93.dro@gmai.com>
2021-04-15 13:08:53 +0200

    fix user action import

--- client/src/components/chat-window/chat-window.js
+++ client/src/components/chat-window/chat-window.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
-import setUser from "../../redux/actions/user-actions";
+import { setUser } from "../../redux/actions/user-actions";
 import {
   setMessages,
   resetMessages,
