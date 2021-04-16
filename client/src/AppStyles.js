6ce80b40ce731d998579579efa4ee22e7afca66b
Anton Drofenik <a93.dro@gmai.com>
2021-04-16 12:45:57 +0200

    remove padding

--- client/src/AppStyles.js
+++ client/src/AppStyles.js
@@ -1,5 +1,9 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const appStyles = makeStyles({});
+const appStyles = makeStyles({
+  appContainer: {
+    padding: 0,
+  },
+});
 
 export default appStyles;
