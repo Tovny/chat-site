96a2026dc4a2730f97c6e1b150a9b4edcea03162
Anton Drofenik <a93.dro@gmai.com>
2021-04-17 13:20:25 +0200

    fix server init

--- server.js
+++ server.js
@@ -428,17 +428,17 @@ const wss = new WebSocket.Server({ server });
           });
       });
     });
+
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static("client/build"));
+
+      app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      });
+    }
+
+    server.listen(PORT, "0.0.0.0");
   } catch (err) {
     console.log(err);
   }
 })();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-server.listen(PORT, "0.0.0.0");
