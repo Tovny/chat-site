0419f88ae77b75c5098b43350de996928d3fee0b
Anton Drofenik <a93.dro@gmai.com>
2021-04-17 10:00:08 +0200

    add drawer support

--- client/src/components/header/Header-styles.js
+++ client/src/components/header/Header-styles.js
@@ -7,8 +7,24 @@ const headerStyles = makeStyles({
     justifyContent: "space-between",
     height: "4rem",
   },
+  roomsOpen: {
+    display: "flex",
+    alignItems: "center",
+  },
+  roomsMenu: {
+    marginRight: "0.25rem",
+  },
+  usersOpen: {
+    display: "flex",
+    alignItems: "center",
+  },
+  usersMenu: {
+    marginLeft: "0.5rem",
+  },
   userAvatar: {
     marginLeft: "0.75rem",
+    height: "2rem",
+    width: "2rem",
   },
 });
 
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,10 +1,13 @@
-import { AppBar, Typography, Toolbar } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+import setOpenRoomDrawer from "../../redux/actions/rooms-drawer-actions";
+import setOpenActiveUsersDrawer from "../../redux/actions/active-users-drawer-actions";
+
+import { AppBar, Button, Typography, Toolbar, Hidden } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
 import headerStyles from "./Header-styles";
 
 import UserControl from "./UserControl";
-
-import { useSelector, useDispatch } from "react-redux";
-import setOpenRoomDrawer from "../../redux/actions/rooms-drawer-actions";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -15,13 +18,33 @@ const Header = () => {
   return (
     <AppBar position="sticky" elevation={0}>
       <Toolbar className={classes.headerToolbar}>
-        <Typography
-          variant="h5"
-          onClick={() => dispatch(setOpenRoomDrawer(true))}
-        >
-          {room}
-        </Typography>
-        <UserControl />
+        <Hidden mdDown>
+          <Typography variant="h5">{room}</Typography>
+        </Hidden>
+        <Hidden mdUp>
+          <Button
+            color="secondary"
+            size="small"
+            onClick={() => dispatch(setOpenRoomDrawer(true))}
+          >
+            <div className={classes.roomsOpen}>
+              <MenuIcon className={classes.roomsMenu} />
+              <Typography variant="subtitle2">{room}</Typography>
+            </div>
+          </Button>
+        </Hidden>
+        <Hidden xsDown>
+          <UserControl />
+        </Hidden>
+        <Hidden smUp>
+          <div className={classes.usersOpen}>
+            <UserControl />
+            <PeopleAltIcon
+              className={classes.usersMenu}
+              onClick={() => dispatch(setOpenActiveUsersDrawer(true))}
+            />
+          </div>
+        </Hidden>
       </Toolbar>
     </AppBar>
   );
