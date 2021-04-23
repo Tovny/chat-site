fbc5a3addc6f614c7d53c5e1fd54f281745cab48
Anton Drofenik <a93.dro@gmai.com>
2021-04-23 09:31:38 +0200

    export last message

--- client/src/redux/reducers/messages.js
+++ client/src/redux/reducers/messages.js
@@ -1,6 +1,9 @@
+export let lastMessage;
+
 const messagesReducer = (state = [], action) => {
   switch (action.type) {
     case "SET_MESSAGES": {
+      lastMessage = action.payload[action.payload.length - 1];
       return [...state, ...action.payload];
     }
     case "RESET_MESSAGES": {
