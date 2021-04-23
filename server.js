8432c2230d5e9d0f102f432e1ed6353fcdcb5b87
Anton Drofenik <a93.dro@gmai.com>
2021-04-23 11:05:02 +0200

    add reconnection handler

--- server.js
+++ server.js
@@ -121,12 +121,12 @@ wss.on("connection", (ws) => {
   });
 });
 
-/*if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}*/
+}
 
 server.listen(PORT);
